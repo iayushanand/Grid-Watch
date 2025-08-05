55c88081fccf2689cd4227d195dccbf05ae73871
iayushanand <clashofserver8823@gmail.com>
2025-08-05 19:24:47 +0530

    updated esp code

--- GridWatchESP/src/main.cpp
+++ GridWatchESP/src/main.cpp
@@ -2,7 +2,7 @@
 #include <HttpClient.h>
 #include <WiFi.h>
 
-
+#define LED_PIN 2
 const char WIFI_SSID[] = "your-wifi-ssid";
 const char WIFI_PASSWORD[] = "your-wifi-password";
 
@@ -12,36 +12,38 @@ void makeRequest();
 void connectWifi();
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
   connectWifi();
 }
 
 void loop() {
   makeRequest();
-  delay(60000);
+  delay(10000);
 }
 
 
 void connectWifi() {
-  Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Starting Connection Request");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    digitalWrite(LED_PIN, HIGH);
     Serial.println("Attemping Connection Request...");
+    digitalWrite(LED_PIN, LOW);
   }
   Serial.print("Connected to WiFi Network with IP: ");
   Serial.println(WiFi.localIP());
-  Serial.end();
 }
 
 
 void makeRequest() {
   HTTPClient http;
-  
-  Serial.begin(9600);
   http.begin(url);
   http.addHeader("X-ApiKey", "your-api-key");
+  digitalWrite(LED_PIN, HIGH);
   int httpCode = http.GET();
+  digitalWrite(LED_PIN, LOW);
 
   if (httpCode > 0) {
     if (httpCode == HTTP_CODE_OK) {
@@ -52,5 +54,4 @@ void makeRequest() {
   }
 
   http.end();
-  Serial.end();
 }
